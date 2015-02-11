2ff6e4064279c20af7b39a52cd263cddc0bb57b9
Tony Alongi <aalongi2@gmail.com>
2015-02-10 23:18:08 -0500

    Added geolocation and load of nearby restaurants
    On page load, finds location, adds it to Google map object, and loads
    the list of restaurants in your zip

--- js/getLocation.js
+++ js/getLocation.js
@@ -1,35 +1,102 @@
+
 //Setup to handle zip code submit.
 window.onload = init;
 function init() {
   var button = document.getElementById("zipcodeSubmit");
   button.onclick = handleSubmit;
+  getMyLocation();
   }
 function handleSubmit() {
   var zipTextInput = document.getElementById("zipcode");
   var zipcode = zipTextInput.value;
-  var url = "http://opentable.herokuapp.com/api/restaurants?zip="+zipcode+"&callback=updateRestaurantList";
-  getRestaurantsNearby(url);
+  getRestaurantsNearby(zipcode);
 }
 //Check if browser supports geolocation API
 function getMyLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(getZipcode);
+    navigator.geolocation.getCurrentPosition(showMap);
   } else {
     alert("Sorry, no browser geolocation support");
   }
 }
 
-//TODO: Get zip code from lat and long position. Probably just display on Googple map object
-function getZipcode(position) {
-    var coordinates = position.coords.latitude + ',' + position.coords.longitude;
+var map;
 
+function showMap(position) {
+  var googleLatAndLong = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  var mapOptions = {
+    zoom: 15,
+    center: googleLatAndLong,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var mapDiv = document.getElementById("map");
+  map = new google.maps.Map(mapDiv, mapOptions);
+  var title = "Your Location";
+  var content = "You are here!";
+  //addMarker(map, googleLatAndLong, title, content);
+  getZipCode(googleLatAndLong);
+}
+
+function addMarker(map, latlong, title, content) {
+  var markerOptions = {
+    position: latlong,
+    map: map,
+    title: title,
+    clickable: true
+  };
+  var marker = new google.maps.Marker(markerOptions);
+
+  var infoWindowOptions = {
+    content: content,
+    position: latlong
+  };
+
+  var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
+
+  google.maps.event.addListener(marker, "click", function() {
+    infoWindow.open(map);
+  });
+}
+
+//Get zip code from lat and long position. Displays best guess location on map.
+function getZipCode(coords) {
+    var infowindow = new google.maps.InfoWindow();
+    var geocoder = new google.maps.Geocoder();
     var div = document.getElementById("location");
-    div.innerHTML = "Your zip code is ";
+
+    //Ping Google geocode API with lat and long for reverse lookup
+    geocoder.geocode({'latLng': coords}, function(results, status){
+      if (status == google.maps.GeocoderStatus.OK){
+        if (results[0]) {
+          map.setZoom(15);
+          marker = new google.maps.Marker({
+            position: coords,
+            map: map
+          });
+          infowindow.setContent(results[0].formatted_address);
+          infowindow.open(map, marker);
+
+          //Look for zip code in results array
+          var zip = '';
+          for (var i=0, len=results[0].address_components.length; i<len; i++) {
+            var ac = results[0].address_components[i];
+            if (ac.types.indexOf('postal_code') >= 0) zip = ac.long_name;
+          }
+          if (zip != '') {
+            div.innerHTML ='<p>We guess your zip code is ' + zip + "</p>";
+            getRestaurantsNearby(zip);
+          }
+        }
+      } else {
+        alert("Geocoder failed due to: " + status);
+      }
+    });
 
 }
 
 
-function getRestaurantsNearby(url) {
+function getRestaurantsNearby(zipcode) {
+  var url = "http://opentable.herokuapp.com/api/restaurants?zip="+zipcode+"&callback=updateRestaurantList";
   var newScriptElement = document.createElement("script");
   newScriptElement.setAttribute("src", url);
   newScriptElement.setAttribute("id", "jsonp");
